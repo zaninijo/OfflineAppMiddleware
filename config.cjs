caf4bd71b0fdf6e8f65866a70918fc5f71e2d635
zanijo <zanini.joaop@gmail.com>
2024-11-21 11:25:57 -0300

    moved API cretendials to .env

--- config.cjs
+++ config.cjs
@@ -1,5 +1,15 @@
 // o node faz birra com importação de JSON em ESM, então tive que criar esse CJS para exportar as configurações.
+// já aproveitei esse script para adicionar as chaves de API
 
-const config = require("./config.json");
+require('dotenv').config();
+
+const keys = JSON.parse(process.env.KEYS)
+const cfg = require("./config.json")
+
+const config = {
+    ...cfg,
+    keys
+};
+
 
 module.exports = config;
